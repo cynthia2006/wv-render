d646189a148df561c5494cb323b0e0fa819ef510
cynthia2006 <121848440+cynthia2006@users.noreply.github.com>
2024-08-22 01:01:20 +0530

    Eliminate soundfile dependency as PyAV is sufficient (and better)
    Although it does eliminate the dependency, the transition is not
    seamless atleast for now. Due to lack of overlapping, the framerate is
    choppier and the FFT is inaccurate (von Hann window requires 50%
    overlap).

--- wv_render/__init__.py
+++ wv_render/__init__.py
@@ -1,5 +1,3 @@
-import soundfile as sf
-
 import pyfftw
 import pyfftw.builders
 
@@ -10,7 +8,7 @@ import skia
 import argparse
 
 from os.path import splitext
-from .ffmpeg import FFmpegOutput
+from .ffmpeg import FFmpegInput, FFmpegOutput
 
 parser = argparse.ArgumentParser()
 parser.add_argument('INPUT', type=argparse.FileType('rb'))
@@ -24,6 +22,7 @@ parser.add_argument('--stroke-width', type=float, default=2, help='stroke width 
 parser.add_argument('--gain', type=float, default=2, help='spectral gain (increases peak height)')
 
 args = parser.parse_args()
+output_file = splitext(args.INPUT.name)[0] + '.mkv'
 
 # Ideally, we should be using OpenGL accelarated Skia rendering. Although it requires,
 # significant amount of biolerplate code. The OpenGL context could be created using
@@ -41,73 +40,66 @@ paint = skia.Paint(
 )
 path = skia.Path()
 bg_color = args.background_color
+margin = 0.05 * args.width  # 0.05vw
 
 # Hanning window with 50% overlap (optimal)
 hann = np.hanning(2048)
 
-margin = 0.05 * args.width  # 0.05vw
+ffmpeg_input = FFmpegInput(args.INPUT.name)
+ffmpeg_output = FFmpegOutput(output_file, args.width, args.height, fps=48000/2048)
 
-with sf.SoundFile(args.INPUT) as f:
-    # TODO Frame synchronization is a problem (i.e. the rate which blocks are read
-    # might not match the desired framerate)
-    samples = pyfftw.empty_aligned((2048, f.channels), dtype='float', n=16)
+# TODO Frame synchronization is a problem (i.e. the rate which blocks are read
+# might not match the desired framerate)
+samples = pyfftw.empty_aligned(2048, dtype='float', n=16)
+bins = np.empty(1025, dtype='float')
+
+fft = pyfftw.builders.rfft(samples, axis=0)
+bin_width = 2048 / 48000
+
+min_bin = int(bin_width * args.min_frequency)
+max_bin = int(bin_width * args.max_frequency) # (F = n * Fs/N)
+
+# TODO What should the first point be?
+points = [skia.Point(0, 0) for _ in range(max_bin - min_bin)]
+
+height = args.height
+coeff1 = height / 1024 * args.gain
+xstep = (args.width - 2 * margin) / (max_bin - min_bin)
+
+print(len(points), min_bin, max_bin, bin_width)
+
+surface = skia.Surface.MakeRaster(surface_info)
+
+with surface as canvas:
+    for block in ffmpeg_input.blocks():
+        samples[:] = block
+
+        # TODO Add overlapping (PyAV won't allow us to!)
+        np.multiply(samples, hann, out=samples)
+
+        transform = fft(samples)
+
+        np.abs(transform, out=bins)
+
+        canvas.drawColor(bg_color)
+        path.reset()
+
+        x = margin
+        i = 1
+        sign = 1 # This creates a wavy effect (Adobe AE)
+
+        # Skip the DC coefficient, which is just the average of all bin values.
+        for i, v in enumerate(bins[min_bin:max_bin]):
+            points[i].set(x, height/2 - sign * coeff1 * v/2)
+            x += xstep
+            sign *= -1
+
+        path.addPoly(points, False)
+
+        canvas.drawPath(path, paint)
+        canvas.peekPixels(pixmap)
+
+        ffmpeg_output.write_pixmap(pixmap)
     
-    bins = np.empty((1025, f.channels), dtype='float')
-    bars = np.empty(1025, dtype='float')
-
-    fft = pyfftw.builders.rfft(samples, axis=0)
-    bin_width = 2048 / f.samplerate
-    
-    min_bin = int(bin_width * args.min_frequency)
-    max_bin = int(bin_width * args.max_frequency) # (F = n * Fs/N)
-
-    # TODO What should the first point be?
-    points = [skia.Point(0, 0) for _ in range(max_bin - min_bin)]
-    
-    # TODO Either allow user to customize the FFmpeg options, or get rid of FFmpeg
-    # process = setup_ffmpeg_for_output(args.INPUT.name, args.width, args.height, f.samplerate/1024)
-
-    output_file = splitext(args.INPUT.name)[0] + '.mkv'
-    ffmpeg_output = FFmpegOutput(output_file, args.width, args.height, fps=f.samplerate/1024)
-
-    height = args.height
-    coeff1 = height / (1024 * f.channels) * args.gain
-    xstep = (args.width - 2 * margin) / (max_bin - min_bin)
-
-    with skia.Surface.MakeRaster(surface_info) as canvas:
-        for _ in f.blocks(out=samples, overlap=1024):
-            for c in range(f.channels):
-                np.multiply(samples[:,c], hann, out=samples[:,c])
-
-            transform = fft(samples)
-            
-            # This unconventional use is so that temporary copies aren't created,
-            # as they are, by virtue, quite expensive
-            np.abs(transform, out=bins)
-            np.sum(bins, axis=1, out=bars)
-
-            # Reuse the path object
-            path.reset()
-
-            # Clear the screen
-            canvas.drawColor(bg_color)
-
-            x = margin
-            i = 1
-            sign = 1 # This creates a wavy effect (Adobe AE)
-
-            # Skip the DC coefficient, which is just the average of all bin values.
-            for i, v in enumerate(bars[min_bin:max_bin]):
-                points[i].set(x, height/2 - sign * coeff1 * v/2)
-                x += xstep
-                sign *= -1
-
-            path.addPoly(points, False)
-
-            canvas.drawPath(path, paint)
-            canvas.peekPixels(pixmap)
-
-            ffmpeg_output.write_pixmap(pixmap)
-        
-    # Cleanup
-    ffmpeg_output.flush()
+# Cleanup
+ffmpeg_output.flush()
--- wv_render/ffmpeg.py
+++ wv_render/ffmpeg.py
@@ -1,11 +1,14 @@
 import av
+import av.audio.fifo
 import av.video
 import av.video.reformatter
+import av.audio
+import av.audio.resampler
 
 from os.path import splitext
 
 class FFmpegOutput:
-    def __init__(self, file: str, width: int, height: int, fps: int | float):
+    def __init__(self, file, width: int, height: int, fps: int | float):
         _, ext = splitext(file)      
         if ext != '.mkv':
             raise NotImplementedError
@@ -22,10 +25,14 @@ class FFmpegOutput:
         self.stream = stream
         self.frame = av.video.frame.VideoFrame(width, height, 'rgb0')
         self.scaler = av.video.reformatter.VideoReformatter()
+        self.pts = 0
 
     def write_pixmap(self, pixmap):
         self.frame.make_writable()
+        self.frame.pts = self.pts
         self.frame.planes[0].update(pixmap)
+
+        self.pts += 1
 
         # We are primarily concerned with the pixel format conversion
         new_frame = self.scaler.reformat(self.frame,
@@ -39,3 +46,49 @@ class FFmpegOutput:
     def flush(self):
         for packet in self.stream.encode():
             self.container.mux(packet)
+
+class FFmpegInput:
+    def __init__(self, file, stream: int = 0, length: int = 2048, overlap: int = 1024):
+        container = av.open(file)
+
+        self.resampler = av.audio.resampler.AudioResampler(format='flt',
+                                                           layout='mono',
+                                                           rate=48000,
+                                                           frame_size=length)
+        self.fifo = av.audio.fifo.AudioFifo()
+        self.length = length
+        self.overlap = overlap
+        self.stream = container.streams.audio[stream]
+        self.decoded_stream = container.decode(audio=stream)
+
+    def blocks(self):
+        # Total frames read from the FIFO queue (not in total)
+        frames_read = 0
+        hit_eof = False
+        
+        while True:
+            for frame in self.fifo.read_many(self.length, partial=hit_eof):
+                frames_read += 1
+                yield frame.to_ndarray()[0]
+
+            if frames_read > 0 and not hit_eof:
+                frames_read = 0
+
+                if hit_eof:
+                    break
+                # continue
+            else:
+                if hit_eof:
+                    break
+
+                try:
+                    decoded_frame = next(self.decoded_stream)
+                    resampled_frames = self.resampler.resample(decoded_frame)
+
+                    for frame in resampled_frames:
+                        frame.pts = None
+                        self.fifo.write(frame)
+                except StopIteration:
+                    hit_eof = True
+                
+
